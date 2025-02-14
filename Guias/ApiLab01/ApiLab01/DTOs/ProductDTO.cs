1a0b36b724205529b30798c6496ea0c117067f87
Ivaname99 <mg0913032023@unab.edu.sv>
2025-02-14 17:39:26 -0600

    Creacion del modelo de productos

---
+++ Guias/ApiLab01/ApiLab01/DTOs/ProductDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiLab01.DTOs
+{
+    public class ProductDTO
+    {
+        [Required]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Nombre { get; set; }
+
+        [Required]
+        [Range(0.01, (double)Decimal.MaxValue)]
+        public decimal Precio { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int Stock { get; set; }
+    }
+}
