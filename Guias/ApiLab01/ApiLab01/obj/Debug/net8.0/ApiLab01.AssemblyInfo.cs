a8eab741d3ab9c7a668b1f826d4b2d5db9dd14e4
Ivaname99 <mg0913032023@unab.edu.sv>
2025-02-19 09:02:03 -0600

    Ultimos ajustes, le eliminé un archivo que trae por defecto

--- Guias/ApiLab01/ApiLab01/obj/Debug/net8.0/ApiLab01.AssemblyInfo.cs
+++ Guias/ApiLab01/ApiLab01/obj/Debug/net8.0/ApiLab01.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiLab01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb85812eb997c5eb7a3cab0817153f7203336bae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4cbf8097d3c4ff0a043c076562cda6bc604580")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiLab01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ApiLab01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
